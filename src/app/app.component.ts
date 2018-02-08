48ec1ff4b265f13cff97a5aea1a048c064da359a
Oliver Ganz <oli@testingtime.com>
2018-02-08 15:26:55 +0100

    Show language changer in the footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Inject, LOCALE_ID} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,8 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(@Inject(LOCALE_ID) private localeId: string) {}
 
   languages = [
     { code: 'en', label: 'English'},
