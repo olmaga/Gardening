25e510a3ba73b0640748f6d352f9c5e43539c034
Oliver Ganz <oli@testingtime.com>
2018-02-08 10:17:36 +0100

    Add more test cases to the species list and app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app.component';
 import {SpeciesListComponent} from './species-list/species-list.component';
 import {TodoService} from './todo.service';
 import {SpecieService} from './specie.service';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,9 +17,18 @@ describe('AppComponent', () => {
       ]
     }).compileComponents();
   }));
+
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Garden your ass off!');
+  }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
 });
--- src/app/species-list/species-list.component.spec.ts
+++ src/app/species-list/species-list.component.spec.ts
@@ -3,29 +3,45 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {SpeciesListComponent} from './species-list.component';
 import {SpecieService} from '../specie.service';
 import {TodoService} from '../todo.service';
+import {AppComponent} from '../app.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('SpeciesListComponent', () => {
   let component: SpeciesListComponent;
   let fixture: ComponentFixture<SpeciesListComponent>;
+  let element: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SpeciesListComponent],
+      declarations: [
+        AppComponent,
+        SpeciesListComponent
+      ],
       providers: [
         SpecieService,
         TodoService
       ]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SpeciesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    element = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h2 tag', async(() => {
+    expect(element.nativeElement.querySelector('h2').textContent).toContain('Available Species:');
+  }));
+
+  it('should contain a list', async(() => {
+    expect(element.nativeElement.querySelector('ul')).toBeTruthy();
+  }));
+
 });
